86e57800f020119a94824bb3805753b1f88d4828
AngerOverApathy <violasian@gmail.com>
2022-08-26 19:01:47 -0700

    creating js to loop through images

--- app.js
+++ app.js
@@ -2,35 +2,37 @@ const albumContainer = document.getElementById('fg-album')
 const fantAlbum = "./assets/img-optimized/fant-glam/"
 
 const fileNames = [
-'fair1.jpg',
+'fair1-horizontal.jpg',
 'fair2.jpg',
-'fair3.jpg',
-'fair4.jpg',
 'fair5.jpg',
 'fair6.jpg',
+'fair9-horizontal.jpg',
 'fair7.jpg',
-'fair8.jpg',
-'fair9.jpg',
-'fair10.jpg',
-'fair11.jpg',
 'fair12.jpg',
+'fair4.jpg',
+'fair10-horizontal.jpg',
+'fair11.jpg',
+'fair8-horizontal.jpg',
+'fair3.jpg',
+'trix1-horizontal.jpg',
 'gat1.jpg',
-'gat2.jpg',
+'trix5.jpg',
+'trix2.jpg',
 'gat3.jpg',
+'gat2-horizontal.jpg',
 'gat4.jpg',
+'trix4.jpg',
+'trix3.jpg',
 'gat5.jpg',
 'gat6.jpg',
-'trix1.jpg',
-'trix2.jpg',
-'trix3.jpg',
-'trix4.jpg',
-'trix5.jpg',
-'trix6.jpg']
+'trix6-horizontal.jpg']
     const fantArr = []
 
 const images = fileNames.map(x => {
+    const [a,b] = x.split('-')
     return {
-        url: fantAlbum + x
+        url: fantAlbum + x,
+        orientation: b ? b.split('.')[0] : ''
     }
 });
 
